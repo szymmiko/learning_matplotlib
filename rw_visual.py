28d46f1af5654cff17c883b85e78f84e024ad025
Szymon Mikolajczyk <szpmikolajczyk@gmail.com>
2025-01-03 18:02:24 +0100

    Added the RandomWalk class in random_walk.py for generating random walks. Created a series of plots in rw_visual.py to visualize random walks, including point color mapping and distinct settings for start and end points. Added features for hiding axes and adjusting plot size.

---
+++ rw_visual.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Tworzenie nowego błądzenia losowego, dopóki program pozostaje aktywny.
+while True:
+    # Przygotowanie danych błądzenia losowego.
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
+
+    # Wyświetlenie punktów błądzenia losowego.
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15, 9))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
+               cmap=plt.cm.Blues, edgecolors='none', s=1)
+
+    # Podkreślenie pierwszego i ostatniego punktu błądzenia losowego.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
+               edgecolors='none', s=100)
+
+    # Ukrysie osi.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
+    if keep_running == 'n':
+        break
